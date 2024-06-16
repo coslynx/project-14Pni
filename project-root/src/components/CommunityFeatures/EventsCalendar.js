c45f86fbd3b8979e9812905b3263360e7a08079d
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-16 13:09:24 +0530

    Added: `project-root/src/components/CommunityFeatures/EventsCalendar.js`

---
+++ project-root/src/components/CommunityFeatures/EventsCalendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventsCalendar() {
+  return (
+    <div>
+      <h1>Events Calendar</h1>
+      <p>Display upcoming tournaments and game releases here</p>
+    </div>
+  );
+}
+
+export default EventsCalendar;
