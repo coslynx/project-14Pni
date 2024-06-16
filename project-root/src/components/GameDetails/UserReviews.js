74ddb609e326b842358ec060709d09a2cf062ddc
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-16 13:09:04 +0530

    Added: `project-root/src/components/GameDetails/UserReviews.js`

---
+++ project-root/src/components/GameDetails/UserReviews.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserReviews = () => {
+  // Logic for fetching and displaying user reviews for a specific game
+  return (
+    <div>
+      <h2>User Reviews</h2>
+      <div className="user-reviews-container">
+        {/* Display user reviews here */}
+      </div>
+    </div>
+  );
+};
+
+export default UserReviews;
