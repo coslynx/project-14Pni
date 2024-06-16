7bc406c6cbc3a944bb75ec2eff8d78c13c579c0c
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-16 13:09:18 +0530

    Added: `project-root/src/components/CommunityFeatures/Forums.js`

---
+++ project-root/src/components/CommunityFeatures/Forums.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forums = () => {
+    return (
+        <div>
+            <h1>Welcome to the Forums</h1>
+            <p>Join the discussion, share tips, and connect with other players!</p>
+        </div>
+    );
+};
+
+export default Forums;
